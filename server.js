242aabefa138502a90da4b74753dcec0b4798566
Vu Dinh <isthis.vu@gmail.com>
2016-04-14 08:42:10 -0700

    added server.js, package.json, require express

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var app = express()
+
+app.use(express.static(__dirname + "/static"));
+
+
+port = 5000
+app.listen(port, function(){
+	console.log("LISTEN TO PORT: ",port,"FOR MINI STORE")
+})
